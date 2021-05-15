108928f1abd24257d8a4472d3abf6dc94387345e
Jeffey <slassarg20@gmail.com>
2021-05-14 23:03:09 -0400

    Creating second attemp

---
+++ IsmorphicMaps.groovy
@@ -0,0 +1,16 @@
+class Ismorphic{
+
+
+        def boolean IsIsmorphic (String str1, String str2){
+            
+            
+            return true
+
+        }
+
+    static void main (args){
+            Ismorphic a  = new Ismorphic();
+
+            println(a.IsIsmorphic("aab","xxy"))
+    }
+}

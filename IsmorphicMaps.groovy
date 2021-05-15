e7554bbf0388d2837f4fddb23e405e374e61eb9a
Jeffey <slassarg20@gmail.com>
2021-05-14 23:16:06 -0400

    Final Commit on Second Attemp

--- IsmorphicMaps.groovy
+++ IsmorphicMaps.groovy
@@ -3,6 +3,40 @@ class Ismorphic{
 
         def boolean IsIsmorphic (String str1, String str2){
             
+            int x = str1.length() 
+            int y = str2.length() 
+
+            if(x != y)
+             return false 
+
+            int size = 256 
+            def mp1 = [:]  
+            def mp2 = [:]
+
+            for(int i = 0; i < size ; i++){
+                mp1.put(i,-1)
+                mp2.put(i,false) 
+
+            }
+
+            for(int i = 0; i < y; i++)
+            {
+                int x1 = (int)str1.getAt(i)
+                int y1 = (int)str2.getAt(i) 
+
+                if(mp1.get(x1) == -1)
+                {
+                    if(mp2.get(y1) == true)
+                        return false
+                    mp2.put(x1,true)
+                    mp1.put(x1,y1)
+                }else if(mp1.getAt(x1) != y1)
+                {
+                    return false
+                }
+              
+            }            
+
             
             return true
 
